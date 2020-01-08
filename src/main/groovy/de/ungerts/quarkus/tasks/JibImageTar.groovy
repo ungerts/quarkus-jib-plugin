e49db3269c1c139be722ea49eab6a696f5aefb9e
Tobias Unger <ungerts@gmail.com>
2020-01-08 15:18:46 +0100

    Optimize imports

--- src/main/groovy/de/ungerts/quarkus/tasks/JibImageTar.groovy
+++ src/main/groovy/de/ungerts/quarkus/tasks/JibImageTar.groovy
@@ -4,7 +4,6 @@ import de.ungerts.quarkus.config.QuarkusJibExtension
 import de.ungerts.quarkus.util.JibUtils
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.FileCollection
-import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.Nested
 import org.gradle.api.tasks.OutputFiles
